cb50bef0f984b2a38c3c770cb335b4f0cafcd94a
Jeongbeen Lee <ljb22060@gmail.com>
2025-07-16 21:10:16 +0900

    [Silver III] Title: 안테나, Time: 588 ms, Memory: 42404 KB -BaekjoonHub

---
+++ ljb220/BOJ/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        bw.write(arr[(n - 1) / 2] + "\n");
+        bw.flush();
+    }
+}
