3212ccfd624fff0828e3ada081d9f3e1d879bc60
Jeongbeen Lee <ljb22060@gmail.com>
2025-07-30 21:21:58 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 244 ms, Memory: 79144 KB -BaekjoonHub

---
+++ ljb220/BOJ/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[][] dp = new int[4001][4001];
+        String A = br.readLine();
+        String B = br.readLine();
+        int lenA = A.length();
+        int lenB = B.length();
+        int max = 0;
+
+        for (int i = 1; i <= lenA; i++) {
+            for (int j = 1; j <= lenB; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        bw.write(max + "\n");
+        bw.flush();
+    }
+}
