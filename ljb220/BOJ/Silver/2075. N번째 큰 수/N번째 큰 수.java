5779dc3c1d02471d3ba649491060ef9d60a3832e
Jeongbeen Lee <ljb22060@gmail.com>
2025-09-06 02:21:29 +0900

    [Silver III] Title: N번째 큰 수, Time: 804 ms, Memory: 273448 KB -BaekjoonHub

---
+++ ljb220/BOJ/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        int num = 0;
+        for (int i = 0; i < N; i++) {
+            num = pq.poll();
+        }
+        bw.write(num + "\n");
+        bw.flush();
+    }
+}
